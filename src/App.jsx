3d5ac14ff19a34aa139e562051f559f757500234
Waelragob <93447892+Waelragob@users.noreply.github.com>
2024-11-22 09:31:06 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css';
 import App_Home from './app/app';
 import { ThemeProvider } from './Conteex/ThemeContext';
-import { HashRouter as Router } from 'react-router-dom';
+
 
 function App() {
   return (
-    <Router>
+   
       <ThemeProvider>
         <App_Home />
       </ThemeProvider>
-    </Router>
+  
   );
 }
 
