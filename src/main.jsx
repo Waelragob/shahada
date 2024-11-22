337529035886f39f548754cffb1b76d5abb1072b
Waelragob <93447892+Waelragob@users.noreply.github.com>
2024-11-22 09:38:50 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'regenerator-runtime/runtime';
-import { HashRouter  } from 'react-router-dom';
+import {createHashRouter,RouterProvider} from 'react-router-dom';
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-    <App />
-    </HashRouter>
+     <RouterProvider router={router} />
   </StrictMode>,
 )
