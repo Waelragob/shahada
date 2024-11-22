da03af7a5bd62002fa121edd47909285f1383b7a
Waelragob <93447892+Waelragob@users.noreply.github.com>
2024-11-22 09:30:33 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'regenerator-runtime/runtime';
-
+import { HashRouter  } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <HashRouter>
     <App />
+    </HashRouter>
   </StrictMode>,
 )
